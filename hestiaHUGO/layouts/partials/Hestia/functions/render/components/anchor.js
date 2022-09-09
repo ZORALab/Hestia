62ddc449465759971fc5e8f535026efe0ac20fd9
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-09-09 16:45:40 +0800

    hestiaHUGO: clean up Hestia UI components data path
    Since the existing Hestia UI components data path was directly
    ported from experimentations repository, we have to clean them up
    matching the current layout and functions. Hence, let's do this.
    
    This patch cleans up Hestia UI components data path in hestiaHUGO/
    directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- hestiaHUGO/layouts/partials/Hestia/functions/render/components/anchor.js
+++ hestiaHUGO/layouts/partials/Hestia/functions/render/components/anchor.js
@@ -14,19 +14,4 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */ -}}
-{{- /* INPUT PARAMETERS */ -}}
-{{- /* .  = Page data in Hestia Structure  */ -}}
-
-
-
-
-{{- /* prepare working variables for this function */ -}}
-{{- $Page := partial "Hestia/functions/data/Clean" . -}}
-{{- $dataList := dict -}}
-{{- $ret := false -}}
-
-
-
-
-{{- /* render output */ -}}
-{{- return merge $Page (dict "Components" $dataList) -}}
+{{- /* WARNING: Auto-generated file. DO NOT EDIT! */ -}}
