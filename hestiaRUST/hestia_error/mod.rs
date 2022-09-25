ce7e2e2a89acadefc5c089b66e67d4872741b2d8
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-09-25 21:50:15 +0800

    hestiaRUST: added hestia_error crate
    Due to Rust's special requirement of CamelCase and their very special
    way of handling enumeration list, developing a translator in either
    way is going to be very costly. Hence, we have to manually develop
    the package. Let's do this.
    
    This patch adds hestia_error crate in hestiaRUST/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- hestiaRUST/hestia_error/mod.rs
+++ hestiaRUST/hestia_error/mod.rs
@@ -13,5 +13,207 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-pub mod data_type;
-pub mod list;
+pub enum Error {
+	// Common errors and operations
+	Ok,
+	BadDescriptor,
+	BadExchange,
+	BadExec,
+	BadMount,
+	BadPipe,
+	BadRequest,
+	BadStreamPipe,
+	Cancelled,
+	CleaningRequired,
+	Deadlock,
+	Expired,
+	IllegalByteSequence,
+	IllegalSeek,
+	InvalidArgument,
+	IsEmpty,
+	MaxedExchange,
+	MaxedQuota,
+	MissingLock,
+	NotEmpty,
+	NotPermitted,
+	NotPossible,
+	NotPossibleByRFKill,
+	NotRecoverable,
+	OutOfRange,
+	PermissionDenied,
+	Timeout,
+	TooManyRead,
+	TooManyLoop,
+	TooManyReferences,
+	TooManyLink,
+	TryAgain,
+	Unsupported,
+	WouldBlock,
+
+	// lifecycle states
+	Restart,
+	Resume,
+	Shutdown,
+	Sleep,
+	Stalled,
+	Standby,
+
+	// progress
+	ProgressScheduled,
+	ProgressAlreadyExecuting,
+	ProgressExecuting,
+	ProgressCompleted,
+
+	// tri-tier inter-package communications
+	LV1NotSync,
+	LV1Paused,
+	LV1Reset,
+
+	LV2NotSync,
+	LV2Paused,
+	LV2Reset,
+
+	LV3NotSync,
+	LV3Paused,
+	LV3Reset,
+
+	// data (input/output parameters, type, etc)
+	DataBad,
+	DataEmpty,
+	DataInvalid,
+	DataIsUnique,
+	DataMissing,
+	DataNotUnique,
+	DataOverflow,
+	DataRemoved,
+	DataTooLong,
+
+	// entity (device, file, directory, etc)
+	EntityBad,
+	EntityBusy,
+	EntityDead,
+	EntityExists,
+	EntityFaulty,
+	EntityMissing,
+	EntityMissingChild,
+	EntityOutOfBuffer,
+	EntityPoisoned,
+	EntityReadOnly,
+	EntityTooBig,
+	EntityTooManyOpened,
+	EntityUnattached,
+
+	EntityIsDirectory,
+	EntityIsFile,
+	EntityIsLink,
+	EntityIsSocket,
+
+	EntityIsNotDirectory,
+	EntityIsNotFile,
+	EntityIsNotLink,
+	EntityIsNotSocket,
+
+	EntityRemoteChanged,
+	EntityRemoteError,
+	EntityRemoteIO,
+
+	EntityMissingStreamableResources,
+	EntityNotStreamable,
+	EntityStreamable,
+
+	EntityATypewriter,
+	EntityNotATypewriter,
+
+	EntityBadDescriptor,
+	EntityFiletableOverflow,
+
+	// key (cryptography)
+	KeyBad,
+	KeyDestroyed,
+	KeyExpired,
+	KeyMissing,
+	KeyRejected,
+	KeyRevoked,
+
+	// library
+	LibraryBad,
+	LibraryCorrupted,
+	LibraryExecFailed,
+	LibraryMaxed,
+	LibraryMissing,
+
+	// network
+	NetworkBad,
+	NetworkBadAd,
+	NetworkDown,
+	NetworkNotConnected,
+	NetworkReset,
+	NetworkRFS,
+	NetworkUnreachable,
+
+	NetworkHostDown,
+	NetworkHostUnreachable,
+	NetworkSocketUnsupported,
+
+	NetworkAddressInUse,
+	NetworkAddressUnavailable,
+
+	NetworkConnAborted,
+	NetworkConnIsConnected,
+	NetworkConnMissingDestAddress,
+	NetworkConnMultihop,
+	NetworkConnNotConnected,
+	NetworkConnRefused,
+	NetworkConnReset,
+
+	NetworkPayloadBad,
+	NetworkPayloadEmpty,
+	NetworkPayloadMissing,
+	NetworkPayloadTooLong,
+
+	// protocol
+	ProtocolAddressUnsupported,
+	ProtocolBad,
+	ProtocolFamilyUnsupported,
+	ProtocolMissing,
+	ProtocolUnsupported,
+
+	// system (e.g. os, interactable system)
+	SystemBadIO,
+	SystemDeviceCrossLink,
+	SystemInterruptCall,
+	SystemInvalid,
+	SystemMissingBlockDevice,
+	SystemMissingDevice,
+	SystemMissingIO,
+	SystemMissingProcess,
+	SystemOutOfDomain,
+	SystemOutOfMemory,
+	SystemOutOfSpace,
+	SystemReadOnlyFilesystem,
+
+	// user
+	UserAccessBanned,
+	UserAccessLocked,
+	UserAccessNotVerified,
+	UserAccessRejected,
+	UserAccessRevoked,
+
+	UserIDBad,
+	UserIDExists,
+	UserIDMissing,
+	UserMFABad,
+	UserMFAExpired,
+	UserMFAMissing,
+	UserPasswordBad,
+	UserPasswordExpired,
+	UserPasswordMissing,
+	UserKeyfileBad,
+	UserKeyfileExpired,
+	UserKeyfileMissing,
+
+	UserAccessTokenBad,
+	UserAccessTokenExpired,
+	UserAccessTokenMissing,
+	UserAccessTokenRevoked,
+}
