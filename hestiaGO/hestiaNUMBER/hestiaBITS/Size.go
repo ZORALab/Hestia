aac2b9ce469c40d4576b6420496877d80be80f5f
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-09-27 21:59:18 +0800

    hestiaGO: added MatchSize function for hestiaBITS package
    Since we need a function that can resize a parsed uint64 sized
    number into various number data type, we need a bit-wise resize
    function. Hence, let's develop one for both INT type and UINT type.
    
    This patch adds MatchSize function for hestiaBITS package to resize
    a given uint64 number to match any INT or UINT sizes in hestiaGO/
    directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

---
+++ hestiaGO/hestiaNUMBER/hestiaBITS/Size.go
@@ -0,0 +1,66 @@
+// Copyright 2022 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
+// Copyright 2022 ZORALab Enterprise <tech@zoralab.com>
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//                  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package hestiaBITS
+
+import (
+	"hestia/hestiaERROR"
+)
+
+func MatchSize(input *uint64, size uint8, withSign bool) hestiaERROR.Error {
+	var mask uint64
+
+	switch {
+	case input == nil:
+		return hestiaERROR.INVALID_ARGUMENT
+	case size > 64:
+		return hestiaERROR.OUT_OF_RANGE
+	case size == 0:
+		if withSign {
+			mask = MASK_INT64
+		} else {
+			mask = MASK_UINT64
+		}
+	case size <= 8:
+		if withSign {
+			mask = MASK_INT8
+		} else {
+			mask = MASK_UINT8
+		}
+	case size <= 16:
+		if withSign {
+			mask = MASK_INT16
+		} else {
+			mask = MASK_UINT16
+		}
+	case size <= 32:
+		if withSign {
+			mask = MASK_INT32
+		} else {
+			mask = MASK_UINT32
+		}
+	case size <= 64:
+		if withSign {
+			mask = MASK_INT64
+		} else {
+			mask = MASK_UINT64
+		}
+	}
+
+	*input &= mask
+
+	return hestiaERROR.OK
+}
