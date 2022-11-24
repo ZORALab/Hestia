864f912c949ca585ccc54b6d270abb85e409b05b
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-11-24 09:07:13 +0800

    hestiaRUST: added hestia_internal::hestia_math::error.rs library
    Since we need to support hestiaRUST with the hestia_string::Format
    function, we have to translate from hestiaGO. Hence, let's deal with
    hestia_internal::hestia_math::error.rs library.
    
    This patch adds hestia_internal::hestia_math::error.rs library into
    hestiaRUST/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- hestiaRUST/hestia_internal/hestia_math/error.rs
+++ hestiaRUST/hestia_internal/hestia_math/error.rs
@@ -13,11 +13,12 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-pub mod error;
-pub use error::*;
 
-pub mod constants;
-pub use constants::*;
+pub enum Error {
+	ErrorOK,
+	ErrorInputInvalid,
+	ErrorInputOutOfRange,
 
-pub mod division;
-pub use division::*;
+	ErrorBaseInvalid,
+	ErrorBaseMismatched,
+}
