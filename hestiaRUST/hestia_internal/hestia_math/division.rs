84c53161c710b34ba16f467a04a51b4324f2aca1
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-11-24 08:41:58 +0800

    hestiaRUST: added hestia_internal::hestia_math::division.rs
    Since we need to support hestiaRUST with the hestia_string::Format
    function, we have to translate from hestiaGO. Hence, let's start
    with hestia_internal::hestia_math::constants.rs library.
    
    This patch adds hestia_internal::hestia_math::constants.rs library
    into hestiaRUST/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- hestiaRUST/hestia_internal/hestia_math/division.rs
+++ hestiaRUST/hestia_internal/hestia_math/division.rs
@@ -1,5 +1,7 @@
 // Copyright 2022 "Holloway" Chew, Kean Ho <kean.ho.chew@zoralab.com>
 // Copyright 2022 ZORALab Enterprise <tech@zoralab.com>
+// Copyright 2019 Caleb Spare <cespare@gmail.com>
+// Copyright 2018 Ulf Adams <ulfjack@google.com>
 //
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -13,8 +15,29 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-pub mod constants;
-pub use constants::*;
 
-pub mod division;
-pub use division::*;
+macro_rules! factor_of {
+	($name:ident, $uType:ty, $iType:ty) => {
+		pub fn $name(input: $iType, factor: $iType) -> ($uType, $iType, $iType) {
+			if factor == 0 {
+				panic!("factor is 0 where the input is divided by 0");
+			}
+
+			let mut count: $uType = 0;
+			let mut quotient: $iType = input;
+			let mut remainder: $iType;
+			loop {
+				remainder = quotient % factor;
+				quotient /= factor;
+
+				if remainder != 0 {
+					return (count, quotient, remainder);
+				}
+
+				count += 1;
+			}
+		}
+	};
+}
+factor_of!(s32_factor_of, u32, i32);
+factor_of!(s64_factor_of, u64, i64);
