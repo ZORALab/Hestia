d963928272138b6425a435dedf91ba719e226f8b
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2023-04-23 09:22:02 +0800

    hestiaHUGO: refactored server.cmd for minimal code execution
    Since we want to keep things very simple, we can proceed to refactor
    the server.cmd initialization code for less codes. Hence, let's do
    this.
    
    This patch refactors server.cmd for minimal code execution in
    hestiaHUGO/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- hestiaHUGO/server.cmd
+++ hestiaHUGO/server.cmd
@@ -1,99 +1,11 @@
 echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 ################################################################################
-# Linux Variables                                                              #
-################################################################################
-pwd="$PWD"
-################################################################################
-# Linux Library Codes                                                          #
-################################################################################
-_print_status() {
-        __status_message=""
-        case "$1" in
-        warn|Warning|WARNING|warning)
-                __status_message="[ WARNING ]"
-                shift 1
-                ;;
-        err|Error|ERROR|error)
-                __status_message="[  ERROR  ]"
-                shift 1
-                ;;
-        info|Info|INFO)
-                __status_message="[  INFO   ]"
-                shift 1
-                ;;
-        ok|Ok|OK)
-                __status_message="[   OK    ]\n"
-                shift 1
-                ;;
-        *)
-                __status_message="[  INFO   ]"
-                ;;
-        esac
-
-        1>&2 printf "%s %s\n" "$__status_message" "$@"
-        unset __status_message
-}
-
-_exit_main() {
-        case $1 in
-        0)
-                exit 0
-                ;;
-        *)
-                exit 1
-                ;;
-        esac
-}
-
-################################################################################
-# Linux Library Codes                                                          #
-################################################################################
-################################################################################
 # Linux Main Codes                                                             #
 ################################################################################
-check_hugo_availability() {
-        if [ "$(type -p 'hugo')" == "" ]; then
-                _print_status error "missing hugo program."
-                _exit_main 1
-        fi
-}
-
-
-
-
-change_directory_to_hugo_config() {
-        cd "${BASH_SOURCE%/*}"
-}
-
-
-
-
-return_to_current_directory() {
-        cd "$PWD"
-        printf "\n"
-}
-
-
-
-
-run_hugo() {
-        hugo server --noBuildLock \
-                --disableFastRender \
-                --port 8080 \
-                --gc
-}
-
-
-
-
-main() {
-        check_hugo_availability
-        change_directory_to_hugo_config
-        run_hugo
-        return_to_current_directory
-        _exit_main 0
-}
-main $@
+hugo server --noBuildLock \
+        --disableFastRender \
+        --port 8080 \
+        --gc
 ################################################################################
 # Linux Main Codes                                                             #
 ################################################################################
